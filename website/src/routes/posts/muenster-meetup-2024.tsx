89a2b65b6767319c26b35fc11b6545b51dd8496e
Liliana Sanfilippo <liliana.sanfilippo@uni-bielefeld.de>
2025-09-28 21:05:41 +0200

    Münster 24

--- website/src/routes/posts/muenster-meetup-2024.tsx
+++ website/src/routes/posts/muenster-meetup-2024.tsx
@@ -15,8 +15,16 @@ export const metamuensterMeetup2024 = {
 export default function muensterMeetup2024() {
     return (
         <div className={styles.Welcome}>
-
-
+            <p>We were able to present our project PreCyse at the Münster JuniorJam and met a lot of people from BFH again!</p>
+            <p>During the poster exhibition, teams exchanged ideas and discussed their projects. My favourite talk was the ony by David Baker, he presented about his research in the field of protein structure prediction and the design of new proteins.</p>
+            <div className="row align-items-center">
+                <div className="col-5 mx-auto">
+                    <img src="https://media.licdn.com/dms/image/v2/D4E22AQHZixIM3D7WeQ/feedshare-shrink_2048_1536/feedshare-shrink_2048_1536/0/1724261414041?e=1761782400&v=beta&t=YL4swJa-X_SUnVL5UoO16QUXyifxIJ00muxDR_Csmeo"/>
+                </div>
+                <div className="col-5 mx-auto">
+                    <img src="https://media.licdn.com/dms/image/v2/D4E22AQG-CunGJwyWRw/feedshare-shrink_800/feedshare-shrink_800/0/1724261410172?e=1761782400&v=beta&t=04ofbCb_9_aMNKiSpg5gO-dwWuWGZ0ph98hhqrP29Zo"/>
+                </div>
+            </div>
         </div>
     )
 }
