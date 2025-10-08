f66aa0f3fc85d40d298b37fb4bf4dbe22a66f4b0
Liliana Sanfilippo <liliana.sanfilippo@uni-bielefeld.de>
2025-10-09 00:30:39 +0200

    styling etc

--- website/src/components/ArticleSummary.tsx
+++ website/src/components/ArticleSummary.tsx
@@ -9,27 +9,55 @@ import {Card} from "react-bootstrap";
 // &copy;
 
 
-function ArticleSummary({ blogRoot, post, noimg }: {blogRoot: string, post: Post, noimg?: boolean}) {
+function ArticleSummary({ blogRoot, post, noimg, spoiler}: {blogRoot: string, post: Post, noimg?: boolean, spoiler?: boolean}) {
     const image: string = post.image ? post.image : "https://static.igem.wiki/teams/5833/other/placeholder.svg";
   return (
-    <Card className="article-summary">
-        {
-            !noimg ? <><Card.Img  variant="top" src={image} />
-                <small className="px-3 pt-3 img-cc" >
-                    {
-                        post.cc && (  <> Image: {post.cc} </>)
-                    }
-                </small></> : <></>
-        }
-        <Card.Body>
-            <Card.Title> <Link to={`/posts/${post.slug}`}>{post.title}</Link></Card.Title>
-            <Card.Text>
-                <ArticleMeta blogRoot={blogRoot} data={post} />
-                <p>{post.spoiler}</p>
-            </Card.Text>
-        </Card.Body>
-    </Card>
+      <div className={`card mb-3 article-summary`} >
+          <div className="row no-gutters">
+              <div className="col-md-6">
+                  {
+                      !noimg ? <><Card.Img  variant="top" src={image} /></> : <></>
+                  }
+              </div>
+              <div className="col-md-6">
+                  <div className="card-body">
+                      <Card.Title> <Link to={`/posts/${post.slug}`}>{post.title}</Link></Card.Title>
+                      <ArticleMeta blogRoot={blogRoot} data={post} />
+                      {spoiler &&  <p>{post.spoiler}</p>}
+                  </div>
+              </div>
+          </div>
+
+              {
+                  post.cc && (  <small className="px-3 pt-3 img-cc" > Image: {post.cc} </small> )
+              }
+
+      </div>
   )
+}
+
+
+export function ArticleCard({ blogRoot, post, noimg, spoiler}: {blogRoot: string, post: Post, noimg?: boolean, spoiler?: boolean}) {
+    const image: string = post.image ? post.image : "https://static.igem.wiki/teams/5833/other/placeholder.svg";
+    return (
+            <Card className="article-summary">
+                {
+                    !noimg ? <><Card.Img variant="indi" src={image} />
+                        <small className="px-3 pt-3" >
+                            {
+                                post.cc && (  <> Image: {post.cc} </>)
+                            }
+                        </small></> : <></>
+                }
+                <Card.Body>
+                    <Card.Title> <Link to={`/posts/${post.slug}`}>{post.title}</Link></Card.Title>
+                    <Card.Text>
+                        <ArticleMeta blogRoot={blogRoot} data={post} />
+                        <p>{post.spoiler}</p>
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+    )
 }
 
 export default ArticleSummary
--- website/src/routes/about/BlogPage.tsx
+++ website/src/routes/about/BlogPage.tsx
@@ -1,15 +1,20 @@
 import styles from './document.module.css'
 import React from "react";
-import ArticleSummary from "../../components/ArticleSummary";
+import ArticleSummary, {ArticleCard} from "../../components/ArticleSummary";
 import posts from "../posts";
 
 export function BlogPage() {
     return(<div className={styles.Document}>
-        <div className="">
+        <div className="w-75 mx-auto">
             <>
+                <div className="h-100 m-5 p-5">
+                    <div className="col-8 mx-auto" key={posts[0].slug}>
+                        <ArticleCard spoiler blogRoot={"/"} post={posts[0]} />
+                    </div>
+                </div>
                 <div className={'row'}>
                     {posts.map(route => (
-                        <div className="col-3 my-3" key={route.slug}>
+                        <div className="col-4 my-3" key={route.slug}>
                             <ArticleSummary blogRoot={"/"} post={route} />
                         </div>
                     ))}
