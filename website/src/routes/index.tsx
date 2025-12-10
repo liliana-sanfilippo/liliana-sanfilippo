b73d311d3e49ca11f9be278bb7347d31af15866c
Liliana Sanfilippo <liliana.sanfilippo@uni-bielefeld.de>
2025-12-10 22:52:37 +0100

    basename

--- website/src/routes/index.tsx
+++ website/src/routes/index.tsx
@@ -1,11 +1,11 @@
-import React, { Suspense} from 'react'
-import {BrowserRouter, Routes, Route, Navigate, useParams} from 'react-router-dom'
-import BlogLayout from '../components/BlogLayout'
-import BlogIndexPage from '../components/BlogIndexPage'
-import BlogPostLayout from '../components/BlogPostLayout'
+import React, {Suspense} from 'react'
+import {BrowserRouter, Route, Routes, useParams} from 'react-router-dom'
+import BlogLayout from '../components/pageRelated/BlogLayout'
+import BlogIndexPage from '../components/pageRelated/BlogIndexPage'
+import BlogPostLayout from '../components/pageRelated/BlogPostLayout'
 import siteMetadata from '../siteMetadata'
-import { chunk } from 'lodash'
-import NotFoundPage from "../components/NotFoundPage";
+import {chunk} from 'lodash'
+import NotFoundPage from "../components/pageRelated/NotFoundPage";
 import {AboutPage} from "./about/AboutPage";
 import TagIndexPageWrapper from "./TagIndexWrapper";
 import postsWithNav from "./posts";
@@ -13,13 +13,11 @@ import TagPageWrapper from "./tags";
 import {getPathMapping} from "../utils/getPathMapping";
 
 
-
-
-function BlogPostWrapper({ blogRoot }: { blogRoot: string }) {
-    const { slug } = useParams<{ slug: string }>()
+function BlogPostWrapper({blogRoot}: { blogRoot: string }) {
+    const {slug} = useParams<{ slug: string }>()
     const post = postsWithNav.find(p => p.slug === slug)
     if (!post) return <div>Post not found</div>
-    return <BlogPostLayout blogRoot={blogRoot} post={post} />
+    return <BlogPostLayout blogRoot={blogRoot} post={post}/>
 }
 
 function AppRouter() {
@@ -28,72 +26,58 @@ function AppRouter() {
     const pathMapping = getPathMapping();
 
 
-    return (
-        <BrowserRouter>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
+    return (<BrowserRouter basename={"/liliana-sanfilippo"}>
+        <Suspense fallback={<div>Loading...</div>}>
+            <Routes>
 
 
-                    {/* Blog Layout Wrapper */}
-                    <Route element={<BlogLayout  />}>
+                {/* Blog Layout Wrapper */}
+                <Route element={<BlogLayout/>}>
 
 
+                    {/* Index NavigationBar */}
+                    {chunks.map((chunkPosts: any[], i: number) => (i === 0 ? (<Route
+                        key={i}
+                        index
+                        element={<BlogIndexPage
+                            blogRoot="/blog"
+                            postRoutes={chunkPosts}
+                        />}
+                    />) : (<Route
+                        key={i}
+                        path={`page/${i + 1}`}
+                        element={<BlogIndexPage
+                            blogRoot="/blog"
+                            postRoutes={chunkPosts}
+                        />}
+                    />)))}
 
-                        {/* Index NavigationBar */}
-                        {chunks.map((chunkPosts: any[], i:number) => (
-                            i === 0 ? (
-                                <Route
-                                    key={i}
-                                    index
-                                    element={
-                                        <BlogIndexPage
-                                            blogRoot="/blog"
-                                            postRoutes={chunkPosts}
-                                        />
-                                    }
-                                />
-                            ) : (
-                                <Route
-                                    key={i}
-                                    path={`page/${i + 1}`}
-                                    element={
-                                        <BlogIndexPage
-                                            blogRoot="/blog"
-                                            postRoutes={chunkPosts}
-                                        />
-                                    }
-                                />
-                            )
-                        ))}
+                    {/* Posts */}
+                    <Route path="/posts/:slug" element={<BlogPostWrapper blogRoot="/blog"/>}/>
 
-                        {/* Posts */}
-                        <Route path="/posts/:slug" element={<BlogPostWrapper blogRoot="/blog" />} />
-
-                        {/* Misc NavigationBar */}
-                        {/* <Route path="/tags" element={<TagsPage />} />*/}
-                        <Route path="/home" element={<AboutPage />} />
+                    {/* Misc NavigationBar */}
+                    {/* <Route path="/tags" element={<TagsPage />} />*/}
+                    <Route path="/home" element={<AboutPage/>}/>
 
 
-                        {Object.entries(pathMapping).map(([path, { component: Component }]) => (
-                            <Route path={path} element={Component} />
-                        ))}
+                    {Object.entries(pathMapping).map(([path, {component: Component}]) => (
+                        <Route path={path} element={Component}/>))}
 
-                        {/* Tag Index Page */}
-                        <Route path="/tags" element={<TagIndexPageWrapper />} />
-
-
-                        {/* Tag NavigationBar */}
-                        <Route path="/tags/:tag" element={<TagPageWrapper blogRoot="/" />} />
+                    {/* Tag Index Page */}
+                    <Route path="/tags" element={<TagIndexPageWrapper/>}/>
 
 
-                    </Route>
-                    {/* Fallback 404 */}
-                    <Route path="*" element={<NotFoundPage />} />
+                    {/* Tag NavigationBar */}
+                    <Route path="/tags/:tag" element={<TagPageWrapper blogRoot="/"/>}/>
 
-                </Routes>
-            </Suspense>
-        </BrowserRouter>
-    )
+
+                </Route>
+                {/* Fallback 404 */}
+                <Route path="*" element={<NotFoundPage/>}/>
+
+            </Routes>
+        </Suspense>
+    </BrowserRouter>)
 }
 
 
