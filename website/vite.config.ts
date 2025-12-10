1529c017ac3d1df801465c6d09596d8f292405bb
Liliana Sanfilippo <liliana.sanfilippo@uni-bielefeld.de>
2025-12-10 19:39:30 +0100

    base config

--- website/vite.config.ts
+++ website/vite.config.ts
@@ -6,6 +6,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    base: "/",
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
