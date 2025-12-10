33d4c85a00e862626e2f65bcab0e27f6a18e7206
Liliana Sanfilippo <liliana.sanfilippo@uni-bielefeld.de>
2025-12-10 22:50:38 +0100

    html workaround

--- website/src/components/pageRelated/NotFoundPage.tsx
+++ website/src/components/pageRelated/NotFoundPage.tsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import styles from './NotFoundPage.module.css'
 
 // Note that create-react-navi-app will always show an error screen when this
 // is rendered. This is because the underlying react-scripts package shows
@@ -7,11 +6,9 @@ import styles from './NotFoundPage.module.css'
 // by <NotFoundBoundary>. To see the error rendered by this function,
 // you'll just need to close the error overlay with the "x" at the top right.
 function NotFoundPage() {
-  return (
-    <div className={styles.NotFound}>
-      <h1>404 - Not Found</h1>
-    </div>
-  )
+    return (<div>
+            <h1>404 - Not Found (Individual)</h1>
+        </div>)
 }
 
-export default NotFoundPage
+export default NotFoundPage;
